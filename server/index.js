4e0d5ab80ca9feadb682c2d5d8a25658db902a88
FollyWolly <folkertjanvanderpol@gmail.com>
2019-03-28 12:17:23 +0100

    everything is broken

--- server/index.js
+++ server/index.js
@@ -41,4 +41,4 @@ app.get('/paintings/:id', async (req, res) => {
   })
 })
 
-app.listen(process.env.PORT || 3000, ()=> console.log(`listening on ${process.env.PORT} 3000`))
+app.listen(process.env.port || 3000, ()=> console.log(`listening on ${process.env.port || 3000}`))
